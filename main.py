8843bb2e01274cc0856834e659cd82358cf173f8
mr-nsin <singhal.nitin94@gmail.com>
2025-03-19 17:38:38 +0530

    Complete the code for chat app for multi user

--- main.py
+++ main.py
@@ -1,24 +1,76 @@
-from fastapi import FastAPI, WebSocket, Request
+from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
-
+from dataclasses import dataclass
+from typing import Dict
+import uuid
+import json
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 template = Jinja2Templates(directory="templates")
 app = FastAPI()
 
+@dataclass
+class ConnectionManager():
+    def __init__(self) -> None:
+        self.active_connection: dict = {}
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        id = str(uuid.uuid4())
+
+        self.active_connection[id] = websocket
+        data = json.dumps({"isMe": True, "data": "Have joined!!", "username" :"YOU"})
+        await self.send_message(websocket, data)
+
+    async def send_message(self, ws: WebSocket, message: str):
+        await ws.send_text(message)
+
+    async def broadcast(self, ws: WebSocket, data: str):
+        decoded_data = json.loads(data)
+        for connection in self.active_connection.values():
+            is_me = False
+            if (connection == ws):
+                is_me = True
+            await connection.send_text(json.dumps({"isMe": is_me, "data": decoded_data['message'], "username": decoded_data['username']}))
+
+    def find_connection_id(self, ws: WebSocket):
+        websocket_list = list(self.active_connection.values())
+        id_list = list(self.active_connection.keys())
+
+        pos = websocket_list.index(ws)
+
+        return id_list[pos]
+
+    async def disconnect(self, ws: WebSocket):
+        id = self.find_connection_id(ws)
+        del self.active_connection[id]
+
 app.mount('/static', StaticFiles(directory="static"), name="static")
 
-@app.get("/")
+connection_manager = ConnectionManager()
+
+@app.get("/", response_class=HTMLResponse)
 async def get(request: Request):
     return template.TemplateResponse("index.html", {"request": request, 'title': 'Chat app 1'})
 
-@app.websocket("/ws")
+@app.websocket("/message")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    await connection_manager.connect(websocket)
+
+    try:
+        while True:
+            # Receive the texts from the users
+            data = await websocket.receive_text()
+            await connection_manager.broadcast(websocket, data)
+    except WebSocketDisconnect:
+        return RedirectResponse("/")
+    
+    # await websocket.accept()
+    # while True:
+    #     data = await websocket.receive_text()
+    #     await websocket.send_text(f"Message text was: {data}")
 
 if __name__ == '__main__':
     import uvicorn
--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,77 @@
-var ws = new WebSocket("ws://localhost:9090/ws");
-ws.onmessage = function(event) {
-    var messages = document.getElementById('messages')
-    var message = document.createElement('li')
-    var content = document.createTextNode(event.data)
-    message.appendChild(content)
-    messages.appendChild(message)
-};
-function sendMessage(event) {
-    var input = document.getElementById("messageText")
-    ws.send(input.value)
-    input.value = ''
-    event.preventDefault()
+let $ = jQuery;
+let socket;
+
+function initializeWebSocket() {
+  socket = new WebSocket('ws://localhost:9090/message');
+
+  socket.onopen = function (event) {
+    console.log('WebSocket connection established.');
+  };
+
+  socket.onmessage = function (event) {
+    const data = JSON.parse(event.data);
+    const msgClass = data.isMe ? 'user-message' : 'other-message';
+    const sender = data.isMe ? 'You' : data.username;
+    const message = data.data;
+    const messageElement = $('<li>').addClass('clearfix');
+    messageElement.append($('<div>').addClass(msgClass).text(sender + ': ' + message));
+    $('#messages').append(messageElement);
+    $('#chat').scrollTop($('#chat')[0].scrollHeight);
+  };
+
+
+  socket.onerror = function (event) {
+    console.error('WebSocket error. Please rejoin the chat.');
+    showJoinModal();
+  };
+
+  socket.onclose = function (event) {
+    if (event.code === 1000) {
+      console.log('WebSocket closed normally.');
+    } else {
+      console.error('WebSocket closed with error code: ' + event.code + '. Please rejoin the chat.');
+      showJoinModal();
+    }
+  };
+}
+
+function showJoinModal() {
+  $('#username-form').show();
+  $('#chat').hide();
+  $('#message-input').hide();
+  $('#usernameModal').modal('show');
+}
+
+$('#open-modal').click(function () {
+  showJoinModal();
+});
+
+function joinChat() {
+  $('#username-form').hide();
+  $('#chat').show();
+  $('#message-input').show();
+  $('#usernameModal').modal('hide');
+}
+
+$('#join').click(function () {
+  initializeWebSocket();
+  joinChat()
+});
+
+$('#send').click(function () {
+  sendMessage();
+});
+
+$('#message').keydown(function (event) {
+  if (event.key === "Enter") {
+    sendMessage();
+  }
+});
+
+function sendMessage() {
+  const message = $('#message').val();
+  if (message) {
+    socket.send(JSON.stringify({ "message": message, "username": $('#usernameInput').val() }));
+    $('#message').val('');
+  }
 }
